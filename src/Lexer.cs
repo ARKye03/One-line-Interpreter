28f6968b3f8ab125fd23618f36f0c9f6be44863d
ARKye03 <rafa2003sanchez@gmail.com>
2023-07-27 18:32:31 -0400

    0.0.1

--- src/Lexer.cs
+++ src/Lexer.cs
@@ -1,130 +1,199 @@
-//Tokens:
-namespace mini_kompiler;
+//Here is my lexer for my expressions
 
-public class Lexer
-{
-    public Tokenizer tokenizer;
-    public Lexer()
-    {
-        this.tokenizer = new Tokenizer();
+using System;
+
+namespace mini_compiler;
+
+public class Lexer{
+    private string text;
+    private int pos;
+    private int line;
+    private int column;
+    private char current_char;
+    public Lexer(string text){
+        this.text = text;
+        this.pos = 0;
+        this.line = 1;
+        this.column = 1;
+        this.current_char = text[pos];
     }
-    public void lexicalAnalysis(string text)
-    {
-        int line = 1;
-        int column = 1;
-        string value = "";
-        string type = "";
-        for (int i = 0; i < text.Length; i++)
-        {
-            if (text[i] == ' ' || text[i] == '\n')
-            {
-                if (value != "")
-                {
-                    this.tokenizer.addToken(new Token(value, type, line, column));
-                    value = "";
-                    type = "";
-                }
-                if (text[i] == '\n')
-                {
-                    line++;
-                    column = 1;
-                }
-            }
-            else if (text[i] == '+' || text[i] == '-' || text[i] == '*' || text[i] == '/' || text[i] == '=' || text[i] == '>' || text[i] == '<')
-            {
-                if (value != "")
-                {
-                    this.tokenizer.addToken(new Token(value, type, line, column));
-                    value = "";
-                    type = "";
-                }
-                type = "Operador";
-                value += text[i];
-            }
-            else if (text[i] == '!' || text[i] == '&' || text[i] == '|')
-            {
-                if (value != "")
-                {
-                    this.tokenizer.addToken(new Token(value, type, line, column));
-                    value = "";
-                    type = "";
-                }
-                type = "Condicion";
-                value += text[i];
-            }
-            else if (text[i] == '(' || text[i] == ')' || text[i] == '{' || text[i] == '}')
-            {
-                if (value != "")
-                {
-                    this.tokenizer.addToken(new Token(value, type, line, column));
-                    value = "";
-                    type = "";
-                }
-                type = "Parentesis";
-                value += text[i];
-                this.tokenizer.addToken(new Token(value, type, line, column));
-                value = "";
-                type = "";
-                if (text[i] == '\n')
-                {
-                    line++;
-                    column = 1;
-                }
-                else
-                {
-                    column++;
-                }
-            }
-            else if (text[i] == '"')
-            {
-                if (value != "")
-                {
-                    this.tokenizer.addToken(new Token(value, type, line, column));
-                    value = "";
-                    type = "";
-                }
-                type = "Cadena";
-                value += text[i];
-                i++;
-                while (text[i] != '"')
-                {
-                    value += text[i];
-                    i++;
-                }
-                this.tokenizer.addToken(new Token(value, type, line, column));
-                value = "";
-                type = "";
-                if (text[i] == '\n')
-                {
-                    line++;
-                    column = 1;
-                }
-                else
-                {
-                    column++;
-                }
-            }
-            else
-            {
-                value += text[i];
-                type = "Identificador";
-                if (text[i] == '\n')
-                {
-                    line++;
-                    column = 1;
-                }
-                else
-                {
-                    column++;
-                }
-            }
-            if (i == text.Length - 1)
-            {
-                this.tokenizer.addToken(new Token(value, type, line, column));
-            }
+    private void advance(){
+        pos++;
+        column++;
+        if(pos > text.Length - 1){
+            current_char = '\0';
+        }else{
+            current_char = text[pos];
         }
-        this.tokenizer.printTokens();
-        Console.ReadLine();
-        return;
     }
+    private void skip_whitespace(){
+        while(current_char != '\0' && char.IsWhiteSpace(current_char)){
+            advance();
+        }
+    }
+    private Token number(){
+        string result = "";
+        while(current_char != '\0' && char.IsDigit(current_char)){
+            result += current_char;
+            advance();
+        }
+        if(current_char == '.'){
+            result += current_char;
+            advance();
+            while(current_char != '\0' && char.IsDigit(current_char)){
+                result += current_char;
+                advance();
+            }
+            return new Token(TokenType.Literal, result, line, column);
+        }
+        return new Token(TokenType.Literal, result, line, column);
+    }
+    private Token identifier(){
+        string result = "";
+        while(current_char != '\0' && char.IsLetterOrDigit(current_char)){
+            result += current_char;
+            advance();
+        }
+        return new Token(TokenType.Identifier, result, line, column);
+    }
+    private Token string_literal(){
+        string result = "";
+        advance();
+        while(current_char != '\0' && current_char != '"'){
+            result += current_char;
+            advance();
+        }
+        advance();
+        return new Token(TokenType.Literal, result, line, column);
+    }
+    private Token comment(){
+        string result = "";
+        advance();
+        while(current_char != '\0' && current_char != '\n'){
+            result += current_char;
+            advance();
+        }
+        advance();
+        return new Token(TokenType.Comment, result, line, column);
+    }
+    public Token get_next_token(){
+        while(current_char != '\0'){
+            if(char.IsWhiteSpace(current_char)){
+                skip_whitespace();
+                continue;
+            }
+            if(char.IsDigit(current_char)){
+                return number();
+            }
+            if(char.IsLetter(current_char)){
+                return identifier();
+            }
+            if(current_char == '"'){
+                return string_literal();
+            }
+            if(current_char == '/'){
+                return comment();
+            }
+            if(current_char == '+'){
+                advance();
+                return new Token(TokenType.Operator, "+", line, column);
+            }
+            if(current_char == '-'){
+                advance();
+                return new Token(TokenType.Operator, "-", line, column);
+            }
+            if(current_char == '*'){
+                advance();
+                return new Token(TokenType.Operator, "*", line, column);
+            }
+            if(current_char == '/'){
+                advance();
+                return new Token(TokenType.Operator, "/", line, column);
+            }
+            if(current_char == '('){
+                advance();
+                return new Token(TokenType.Punctuation, "(", line, column);
+            }
+            if(current_char == ')'){
+                advance();
+                return new Token(TokenType.Punctuation, ")", line, column);
+            }
+            if(current_char == '{'){
+                advance();
+                return new Token(TokenType.Punctuation, "{", line, column);
+            }
+            if(current_char == '}'){
+                advance();
+                return new Token(TokenType.Punctuation, "}", line, column);
+            }
+            if(current_char == '['){
+                advance();
+                return new Token(TokenType.Punctuation, "[", line, column);
+            }
+            if(current_char == ']'){
+                advance();
+                return new Token(TokenType.Punctuation, "]", line, column);
+            }
+            if(current_char == ';'){
+                advance();
+                return new Token(TokenType.Punctuation, ";", line, column);
+            }
+            if(current_char == ':'){
+                advance();
+                return new Token(TokenType.Punctuation, ":", line, column);
+            }
+            if(current_char == ','){
+                advance();
+                return new Token(TokenType.Punctuation, ",", line, column);
+            }
+            if(current_char == '='){
+                advance();
+                return new Token(TokenType.Operator, "=", line, column);
+            }
+            if(current_char == '<'){
+                advance();
+                return new Token(TokenType.Operator, "<", line, column);
+            }
+            if(current_char == '>'){
+                advance();
+                return new Token(TokenType.Operator, ">", line, column);
+            }
+            if(current_char == '!'){
+                advance();
+                return new Token(TokenType.Operator, "!", line, column);
+            }
+            if(current_char == '&'){
+                advance();
+                return new Token(TokenType.Operator, "&", line, column);
+            }
+            if(current_char == '|'){
+                advance();
+                return new Token(TokenType.Operator, "|", line, column);
+            }
+            if(current_char == '%'){
+                advance();
+                return new Token(TokenType.Operator, "%", line, column);
+            }
+            if(current_char == '^'){
+                advance();
+                return new Token(TokenType.Operator, "^", line, column);
+            }
+            if(current_char == '~'){
+                advance();
+                return new Token(TokenType.Operator, "~", line, column);
+            }
+            if(current_char == '.'){
+                advance();
+                return new Token(TokenType.Operator, ".", line, column);
+            }
+            if(current_char == '\n'){
+                advance();
+                line++;
+                column = 1;
+                continue;
+            }
+            throw new Exception($"Invalid character '{current_char}' at line {line} and column {column}");
+        }
+        return new Token(TokenType.EOF, null, line, column);
+    }    
 }
